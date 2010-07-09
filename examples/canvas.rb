0c0b96671ed0218c772ab591a7c2b099d287a230
Matt Aimonetti <mattaimonetti@gmail.com>
2010-07-08 21:33:39 -0700

    updated the canvas example

--- examples/canvas.rb
+++ examples/canvas.rb
@@ -1,24 +1,35 @@
 framework 'Cocoa'
-require '../graphics'
-include MRGraphics
+require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'graphics')
 
-img = Canvas.for_image(:size => [400,400]) do
-  background(Color.black)
-  
-  white = Color.white
-  fill(white)
-  stroke(0.2)
-  strokewidth(1)   
-  font("Zapfino")
 
-  80.times do 
-    fontsize rand(170)
-    fill(white.copy.darken(rand(0.8)))
-    letters = %W{ g i a n a} 
-    text(letters[rand(letters.size)],
-         rand(width),
-         rand(height))
+class CustomView < NSView
+  include MRGraphics
+
+  def drawRect(rect)
+    canvas = Canvas.for_image(:size => [400,400]) do
+      background(Color.black)
+
+      white = Color.white
+      fill(white)
+      stroke(0.2)
+      strokewidth(1)   
+      font("Zapfino")
+
+      80.times do 
+        fontsize rand(170)
+        fill(white.copy.darken(rand(0.8)))
+        letters = %W{ g i a n a} 
+        text(letters[rand(letters.size)],
+             rand(width),
+             rand(height))
+      end
+    end
+    
+    # set the image viewer
+    img = NSImage.alloc.initWithCGImage(canvas.cgimage, size: NSZeroSize)
+    img.drawAtPoint([0,0], fromRect: NSZeroRect, operation: NSCompositeSourceOver, fraction: 1)
   end
+  
 end
 
 class AppDelegate
@@ -34,7 +45,6 @@ end
 
 app = NSApplication.sharedApplication
 app.delegate = AppDelegate.new
-
 frame  = [0.0, 0.0, 400, 400]
 
 # window
@@ -44,10 +54,9 @@ window = NSWindow.alloc.initWithContentRect(frame,
     defer:false)
 window.delegate = app.delegate
 
-# set the image viewer
-viewer = IKImageView.alloc.initWithFrame(frame)
-viewer.setImage(img.cgimage, imageProperties: {})
-window.contentView.addSubview(viewer)
+# assign a content view instance
+content_view = CustomView.alloc.initWithFrame(frame)
+window.contentView = content_view
 
 # show the window
 window.display
